584f576fcf6685ab218070afe8d4fea850b8fc0b
renanbandeira94 <67717214+Renan-Bandeira@users.noreply.github.com>
2021-02-09 15:51:01 -0300

    developed method getOne

--- src/main/java/com/github/RenanBandeira/citiesapi/CountryResource.java
+++ src/main/java/com/github/RenanBandeira/citiesapi/CountryResource.java
@@ -4,10 +4,13 @@ import com.github.RenanBandeira.citiesapi.countries.Country;
 import com.github.RenanBandeira.citiesapi.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 
 
 @RestController
@@ -23,5 +26,16 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page) {
         return Repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id){
+        Optional<Country> optional = Repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }else
+            return ResponseEntity.notFound().build();
+
     }
 }
